5bf98a9f38dac1eebb3b60b7bd18711e2484d521
Teguh Arifianto <teguholica@gmail.com>
2015-04-23 06:50:52 +0700

    fix image content

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -144,7 +144,7 @@ class PostController extends Controller {
 				$imageHeight = ($imageWidth*$imageInfo->height)/$imageInfo->width;
 
 				//Modify content img to use lazy imageloader
-				$element->class = 'lazy';
+				$element->class = $element->class.' lazy';
 				$element->width = $imageWidth;
 				$element->height = $imageHeight;
 				$element->href = asset('images/generate/'.$file);
